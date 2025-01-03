01cab63198e691e0bef26e1f9d8f8c9bff99908a
abitha000 <abithak000@yahoo.com>
2025-01-03 23:54:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ if MULTIPLE_DATABASE == True:
 else:
     USER_DB_URI = DATABASE_URI # This Db is for User Data Store
     OTHER_DB_URI = environ.get('OTHER_DB_URI', "mongodb+srv://svdtelegram:svdtelegram12345@cluster123.drtbc.mongodb.net/?retryWrites=true&w=majority") # This Db Is For Other Data Store
-    FILE_DB_URI = environ.get('FILE_DB_URI', "") # This Db Is For File Data Store
+    FILE_DB_URI = environ.get('FILE_DB_URI', "mongodb+srv://svdtelegram:svdtelegram12345@cluster123.drtbc.mongodb.net/?retryWrites=true&w=majority") # This Db Is For File Data Store
     SEC_FILE_DB_URI = environ.get('SEC_FILE_DB_URI', "mongodb+srv://svdtelegram:svdtelegram12345@cluster123.drtbc.mongodb.net/?retryWrites=true&w=majority") # This Db is for File Data Store When First Db Is Going To Be Full.
     
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjfilterbot")
